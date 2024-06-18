2aa31d9e78b38f6756952a81872202f2cc7a23cc
lthreen <timothyjohan11@gmail.com>
2024-06-18 08:58:14 +0700

    dotenv

--- src/index.js
+++ src/index.js
@@ -5,3 +5,10 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+
+app.listen(process.env.PORT, async () => {
+    
+    console.log(`Server is running on port ${process.env.PORT}`);
+
+})
