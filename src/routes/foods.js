535de2ae94b303d8712b93edfc2bc5be2830898b
lthreen <timothyjohan11@gmail.com>
2024-06-18 15:12:54 +0700

    belum benar

--- src/routes/foods.js
+++ src/routes/foods.js
@@ -55,4 +55,62 @@ router.post('/add', checkUser, async (req, res) => {
 
 })
 
+
+// Format before and after harus dalam bentuk date YYYY-MM-DD
+// contoh: 2024-6-18
+router.get('/dates', async (req, res) => {
+  const {before, after} = req.query
+  try {
+    const result = await LogFoods.find({
+      date:{
+        $gt: new Date(after),
+        $lt: new Date(before)
+      }
+    })
+    return res.status(200).send(result)
+    
+  } catch (error) {
+    return res.status(500).send(error)
+    
+  }
+  res.send('GET request to the homepage')
+})
+
+
+// INI BELUM BENARRRRR
+async function getDailyNutritionSummary(email) {
+  const startOfToday = new Date();
+  startOfToday.setHours(0, 0, 0, 0);
+  const endOfToday = new Date();
+  endOfToday.setHours(23, 59, 59, 999);
+
+  try {
+      const result = await LogFoods.aggregate([
+          { $match: { email, date: { $gte: startOfToday, $lt: endOfToday } } },
+          { $group: { _id: null, totalCalories: { $sum: "$calories" }, totalProteins: { $sum: "$proteins" }, totalFats: { $sum: "$fats" }, totalCarbs: { $sum: "$carbs" } } },
+          { $project: { _id: 0, totalCalories: 1, totalProteins: 1, totalFats: 1, totalCarbs: 1 } }
+      ]);
+
+      if (result.length > 0) {
+          return result[0];
+      } else {
+          return { totalCalories: 0, totalProteins: 0, totalFats: 0, totalCarbs: 0 };
+      }
+  } catch (error) {
+      console.error("Error fetching daily nutrition summary:", error.message);
+      throw error;
+  }
+}
+
+router.get('/summary/today/:email', async (req, res) => {
+  const { email } = req.params;
+
+  try {
+      const summary = await getDailyNutritionSummary(email);
+      res.json(summary);
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
+});
+
 module.exports = router
