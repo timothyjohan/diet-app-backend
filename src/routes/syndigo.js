c19883b24a06a8cacf3aefdf358fe4fc10671a58
lthreen <timothyjohan11@gmail.com>
2024-06-18 20:37:06 +0700

    autocomplete not complete

--- src/routes/syndigo.js
+++ src/routes/syndigo.js
@@ -1,3 +1,4 @@
+const { default: axios } = require('axios');
 const express = require('express');
 const router = express.Router()
 
@@ -5,9 +6,24 @@ router.get('/', (req, res) => {
   res.send('GET request to the syndigo')
 })
 
-router.get('/autocomplete', (req, res) => {
-    const {query} = req.query
-    res.send('GET request to the homepage')
+router.get('/autocomplete', async (req, res) => {
+    const {q} = req.query
+    try {
+        console.log(q);
+        const result = await axios.get(`https://trackapi.nutritionix.com/v2/search/instant/?query=${q}`,{
+            headers:{
+                "x-app-id":process.env.X_APP_ID,
+                "x-app-key":process.env.X_APP_KEY
+            }
+        })
+        let arrAuto = []
+        return res.status(200).send(result.data.common)
+    } catch (error) {
+        return res.status(500).send(error.message)
+        
+    }
+    
+    
 })
 
 module.exports = router
