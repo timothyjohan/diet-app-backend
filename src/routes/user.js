f3f24eb48dd849d911e545add7fc99dd9d48e265
lthreen <timothyjohan11@gmail.com>
2024-06-18 09:33:34 +0700

    Add user endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,36 @@
 const express = require('express');
 const router = express.Router()
+const Users = require('../../models/Users');
 
 router.get('/', (req, res) => {
   res.send('GET request to the ')
+})
+
+router.get('/getUsers', async (req, res) => {
+  const userTemp = await Users.find()
+  res.send(userTemp)
+})
+
+router.post('/add', async (req, res) => {
+  const {email, password, name, gender} = req.body
+
+  if(!email || !password || !name || !gender){
+    return res.status(400).send('invalid fields')
+  }
+
+  let newUser = {
+    email:email,
+    password:password,
+    name:name,
+    gender:gender
+  }
+  try {
+    const insert = await Users.create(newUser)
+    return res.status(201).send(insert)
+    
+  } catch (error) {
+    return res.status(400).send(`${error}`)
+  }
 })
 
 module.exports = router
