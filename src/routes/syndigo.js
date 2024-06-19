bb6ef8f7d5bf0232121f918051cae017bb8f0003
lthreen <104489847+timothyjohan@users.noreply.github.com>
2024-06-20 00:21:30 +0700

    get Nutritions

--- src/routes/syndigo.js
+++ src/routes/syndigo.js
@@ -9,7 +9,6 @@ router.get('/', (req, res) => {
 router.get('/autocomplete', async (req, res) => {
     const {q} = req.query
     try {
-        console.log(q);
         const result = await axios.get(`https://trackapi.nutritionix.com/v2/search/instant/?query=${q}`,{
             headers:{
                 "x-app-id":process.env.X_APP_ID,
@@ -27,8 +26,38 @@ router.get('/autocomplete', async (req, res) => {
         return res.status(500).send(error.message)
         
     }
-    
-    
+})
+
+router.get('/nutritions', async (req, res) => {
+    const { q } = req.query
+    try {
+        const result = await axios.post(
+            'https://trackapi.nutritionix.com/v2/natural/nutrients/',
+            {
+                query: q
+            },
+            {
+                headers: {
+                    'x-app-id': process.env.X_APP_ID,
+                    'x-app-key': process.env.X_APP_KEY
+                }
+            }
+        );
+        let newResult = {
+            food_name: result.data.foods[0].food_name,
+            serving_qty: result.data.foods[0].serving_qty,
+            serving_unit: result.data.foods[0].serving_unit,
+            calories: result.data.foods[0].nf_calories,
+            fats: result.data.foods[0].nf_total_fat,
+            protein: result.data.foods[0].nf_protein,
+            carbohydrate: result.data.foods[0].nf_total_carbohydrate,
+            img: result.data.foods[0].photo.thumb
+
+        }
+        return res.status(200).send(newResult)
+    } catch (error) {
+        return res.status(500).send(error)
+    }
 })
 
 module.exports = router
