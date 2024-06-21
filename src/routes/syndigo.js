452b176225c15435658239bcf22793b250239c31
lthreen <timothyjohan11@gmail.com>
2024-06-21 12:20:03 +0700

    Update syndigo.js

--- src/routes/syndigo.js
+++ src/routes/syndigo.js
@@ -7,6 +7,27 @@ router.get('/', (req, res) => {
 })
 
 
+router.get('/autocomplete', async (req, res) => {
+    const {q} = req.query
+    try {
+        const result = await axios.get(`https://trackapi.nutritionix.com/v2/search/instant/?query=${q}`,{
+            headers:{
+                "x-app-id":process.env.X_APP_ID,
+                "x-app-key":process.env.X_APP_KEY
+            }
+        })
+        const data = result.data;
+        const commonFoodNames = data.common.map(item => item.food_name);
+        const brandedFoodNames = data.branded.map(item => item.food_name);
+
+        // Combine the food names into one array
+        const allFoodNames = [...commonFoodNames, ...brandedFoodNames];
+        return res.status(200).send(allFoodNames)
+    } catch (error) {
+        return res.status(500).send(error.message)
+        
+    }
+})
 
 router.get('/nutritions', async (req, res) => {
     const { q } = req.query
